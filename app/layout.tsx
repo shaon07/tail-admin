904a539cea43fa988983f299a73638949d99c69b
Shaon07 <alishaon77@gmail.com>
2023-12-13 22:45:27 +0600

    [update] path

---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Loader from "@/components/common/Loader";
+import { useEffect, useState } from "react";
+import "./data-tables-css.css";
+import "./globals.css";
+import "./satoshi.css";
+
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="flex h-screen overflow-hidden">
+              {/* <!-- ===== Sidebar Start ===== --> */}
+              <Sidebar
+                sidebarOpen={sidebarOpen}
+                setSidebarOpen={setSidebarOpen}
+              />
+              {/* <!-- ===== Sidebar End ===== --> */}
+
+              {/* <!-- ===== Content Area Start ===== --> */}
+              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                {/* <!-- ===== Header Start ===== --> */}
+                <Header
+                  sidebarOpen={sidebarOpen}
+                  setSidebarOpen={setSidebarOpen}
+                />
+                {/* <!-- ===== Header End ===== --> */}
+
+                {/* <!-- ===== Main Content Start ===== --> */}
+                <main>
+                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                    {children}
+                  </div>
+                </main>
+                {/* <!-- ===== Main Content End ===== --> */}
+              </div>
+              {/* <!-- ===== Content Area End ===== --> */}
+            </div>
+          )}
+        </div>
+      </body>
+    </html>
+  );
+}
